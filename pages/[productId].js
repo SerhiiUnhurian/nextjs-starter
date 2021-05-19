619aec3d58b055e862a6ee6dbd77ce03d8ed8b59
Serhii Unhurian <unguryan23@gmail.com>
2021-05-19 18:18:19 +0300

    refactor: load data dynamically for ProductDetailPage

--- pages/[productId].js
+++ pages/[productId].js
@@ -11,11 +11,16 @@ const ProductDetailPage = ({ product }) => {
   );
 };
 
-export async function getStaticProps(context) {
-  const { productId } = context.params;
+async function getData() {
   const filePath = path.join(process.cwd() + '/dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { productId } = context.params;
+  const data = await getData();
   const product = data.products.find(product => product.id === productId);
   const notFound = !product;
 
@@ -29,8 +34,12 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map(product => product.id);
+  const paths = ids.map(id => ({ params: { productId: id } }));
+
   return {
-    paths: [{ params: { productId: 'p1' } }],
+    paths,
     fallback: 'blocking',
   };
 }
