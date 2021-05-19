2cbf61e4b889fe9f204666a7c7405b3cca9123d6
Serhii Unhurian <unguryan23@gmail.com>
2021-05-19 16:17:04 +0300

    feat: add static page generation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-export default function HomePage() {
+import path from 'path';
+import fs from 'fs/promises';
+
+export default function HomePage(props) {
+  const { products } = props;
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd() + '/dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: data,
+  };
 }
