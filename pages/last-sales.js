39c6d1918fcfea6dc5f2df7c326965aec1beefbd
Serhii Unhurian <unguryan23@gmail.com>
2021-05-20 13:49:33 +0300

    feat: combine pre-fetching with client-side fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-const LastSalesPage = () => {
-  const [sales, setSales] = useState();
+const LastSalesPage = props => {
+  const [sales, setSales] = useState(props.sales);
 
   const { data, error } = useSWR(
     'https://next-starter-bc9e6-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
   );
-
-  if (error) return <div>failed to load</div>;
-  if (!data || !sales) return <div>loading...</div>;
 
   useEffect(() => {
     if (data) {
@@ -26,6 +23,9 @@ const LastSalesPage = () => {
     }
   }, [data]);
 
+  if (error) return <div>failed to load</div>;
+  if (!data && !sales) return <div>loading...</div>;
+
   return (
     <ul>
       {sales.map(({ id, username, amount }) => (
@@ -38,3 +38,21 @@ const LastSalesPage = () => {
 };
 
 export default LastSalesPage;
+
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://next-starter-bc9e6-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  );
+  const data = await response.json();
+  const sales = [];
+
+  for (const [key, value] of Object.entries(data)) {
+    sales.push({
+      id: key,
+      username: value.username,
+      amount: value.amount,
+    });
+  }
+
+  return { props: { sales }, revalidate: 10 };
+}
