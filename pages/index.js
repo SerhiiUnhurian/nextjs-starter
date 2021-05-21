24e202c739a668b4172506efde7d85f44bca5ad7
Serhii Unhurian <unguryan23@gmail.com>
2021-05-21 12:24:45 +0300

    refactor: switch from server-side to client-side fething on FilteredEventsPage

--- pages/index.js
+++ pages/index.js
@@ -12,27 +12,6 @@ const HomePage = ({ featuredEvents }) => {
 export default HomePage;
 
 export async function getStaticProps() {
-  // const response = await fetch(
-  //   'https://next-starter-bc9e6-default-rtdb.europe-west1.firebasedatabase.app/events.json'
-  // );
-  // const data = await response.json();
-  // if (!data) {
-  //   return {
-  //     notFound: true,
-  //   };
-  // }
-
-  // const featuredEvents = [];
-
-  // for (const [id, event] of Object.entries(data)) {
-  //   if (event.isFeatured) {
-  //     featuredEvents.push({
-  //       id,
-  //       ...event,
-  //     });
-  //   }
-  // }
-
   const featuredEvents = await getFeaturedEvents();
 
   return {
