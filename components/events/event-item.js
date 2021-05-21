c6fca073030fbba09fa67b2e0c76bf9261e72eb2
Serhii Unhurian <unguryan23@gmail.com>
2021-05-21 14:15:30 +0300

    refactor: optimize images

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import classes from './event-item.module.css';
 import Button from '../ui/button';
+import Image from 'next/image';
 
 const EventItem = props => {
   const { title, image, date, location, id } = props;
@@ -16,7 +17,7 @@ const EventItem = props => {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt="" />
+      <Image src={'/' + image} alt="" width="250" height="160" />
       <div className={classes.content}>
         <div className={classes.summery}>
           <h2>{title}</h2>
