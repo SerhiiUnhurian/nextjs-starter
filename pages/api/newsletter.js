50d6fd08bb5ca9d1bb771403c8d243ff6f136fb2
Serhii Unhurian <unguryan23@gmail.com>
2021-05-23 13:19:41 +0300

    feat: add mongodb to store emails

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,9 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+const url =
+  'mongodb+srv://serhii:391q6pM1Mk65WXJD@cluster0.4v8ry.mongodb.net/newsletters?retryWrites=true&w=majority';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email } = req.body;
 
@@ -7,7 +12,12 @@ function handler(req, res) {
       return res.status(422).json({ message: 'Invalid email address.' });
     }
 
-    console.log(email);
+    const client = await MongoClient.connect(url);
+    const db = client.db();
+    const collection = db.collection('emails');
+    await collection.insertOne({ email });
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
